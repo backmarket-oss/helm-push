cf32f5680520dbf07ef0a345a8eea9138318b9c6
Yannick Lorenzati <yannick.lorenzati@backmarket.com>
2022-11-11 16:01:57 +0100

    fix install link

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -38,11 +38,11 @@ esac
 
 
 if [ "$(uname)" = "Darwin" ]; then
-    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push_${version}_darwin_${arch}.tar.gz"
+    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push-cloudflare-access_${version}_darwin_${arch}.tar.gz"
 elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push_${version}_linux_${arch}.tar.gz"
+    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push-cloudflare-access_${version}_linux_${arch}.tar.gz"
 else
-    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push_${version}_windows_${arch}.tar.gz"
+    url="https://github.com/backmarket-oss/helm-push-cloudflare-access/releases/download/v${version}/helm-push-cloudflare-access_${version}_windows_${arch}.tar.gz"
 fi
 
 echo $url
